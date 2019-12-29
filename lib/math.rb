9b273e53ee9900eb472404f3f936cd0b8bce4b94
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2019-12-29 08:08:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,40 @@
 def addition(num1, num2)
-
+puts sum = num1+num2 
+return sum
 end
+
+addition(5,4)
 
 def subtraction(num1, num2)
-
+puts difference = num1-num2 
+return difference
 end
+
+subtraction(10,5)
 
 def division(num1, num2)
-
+puts quotient = num1/num2
+return quotient
 end
+division(50,2)
 
 def multiplication(num1, num2)
-
+puts product = num1*num2
+return product
 end
+
+multiplication(4,30)
 
 def modulo(num1, num2)
-
+puts remainder =num1%num2
+return remainder
 end
+
+modulo(34,5)
 
 def square_root(num)
-
+puts square_root(num)
+return square_root
 end
+
+square_root(81)
